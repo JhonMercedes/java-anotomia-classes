e6fd9ef5ae6c0feead6442d6a3bde867bd69c2cb
JhonMercedes <jhonzeras93@gmail.com>
2023-09-05 23:28:00 -0300

    aula operadores

---
+++ src/Operadores.java
@@ -0,0 +1,42 @@
+public class Operadores {
+    public static void main (String [] argos) {
+        // String concatenacao = "?";
+
+        // concatenacao = 1+1+1+"1";
+
+        // System.out.println(concatenacao);
+
+        // concatenacao = 1+"1"+1+1;
+
+        // System.out.println(concatenacao);
+
+        // concatenacao = "1"+1+1+1;
+
+        // System.out.println(concatenacao);
+
+        // concatenacao = "1"+(1+1+1);
+
+        // System.out.println(concatenacao);
+
+        // int numero = 5;
+
+        // numero ++;
+        //numero = numero +2 ; 
+
+        //System.out.println(numero);
+
+        // boolean variavel = true;
+
+        // System.out.println(!variavel);
+
+
+        int num1 = 1;
+        int num2 = 2;
+
+        boolean simNao = num1 == num2;
+        System.out.println("igual " + simNao);
+
+        simNao = num1 != num2;
+        System.out.println("Diferente "+ simNao);
+    }
+}
---
+++ src/TIposVariaveis.java
@@ -0,0 +1,9 @@
+public class TIposVariaveis {
+    public static void main (String [] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        String meuNome = "Jhon Armando";
+
+        final double VALOR_PI = 3.14;
+    }
+}
